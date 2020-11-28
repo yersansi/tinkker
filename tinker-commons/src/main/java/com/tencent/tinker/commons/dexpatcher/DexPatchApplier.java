a739df571dbf73cd21552ef4560e746426768ff5
tom <tom@tt.ab.c>
2020-11-28 13:53:18 +0700

    log

--- tinker-commons/src/main/java/com/tencent/tinker/commons/dexpatcher/DexPatchApplier.java
+++ tinker-commons/src/main/java/com/tencent/tinker/commons/dexpatcher/DexPatchApplier.java
@@ -60,7 +60,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
-
+import android.util.Log;
 /**
  * Created by tangyinsheng on 2016/6/30.
  */
